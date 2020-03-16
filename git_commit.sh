13967c6c9ea1690b332ac1463d2cd1b1f2735b5e
gwillig <gustavwillig@gmail.com>
2020-03-16 21:30:10 +0000

    FIX | .gitignor | rename file

--- git_commit.sh
+++ git_commit.sh
@@ -1,4 +1,4 @@
 git add --all;
-git commit -m "FIX | folder examples | not need"
-GIT_COMMITTER_DATE="17 March 2019 at 21:30:10  GMT" git commit --amend --no-edit --date "17 March 2019 at 21:30:10  GMT";
-git push origin master;
+git commit -m "FIX | .gitignor | rename file"
+GIT_COMMITTER_DATE="16 March 2020 at 21:30:10  GMT" git commit --amend --no-edit --date "16 March 2020 at 21:30:10  GMT";
+git push origin master
